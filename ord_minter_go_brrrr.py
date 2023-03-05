68851bf7cd0d33ff11b6e1b036d0632b4034dfff
itsXactlY <6356217+itsXactlY@users.noreply.github.com>
2023-03-05 03:03:54 +0100

    bugfixes

--- ord_minter_go_brrrr.py
+++ ord_minter_go_brrrr.py
@@ -7,32 +7,28 @@ import requests
 import schedule
 
 # preparing alerts (quick 'n dirty right now)
-url = "https://discord.com/api/webhooks/create/your-own-h00ka"
-stub_message = f"BTC monke(s) found new home!"
-embed = {
-    "description": stub_message,
-    "title": "ATTENTION PLEASE!"
-    }
-data = {
-    "embeds": [
-        embed
-        ],
-}
-headers = {
-"Content-Type": "application/json"
-}
+hook = 'https://discord.com/api/webhooks/create/your-own-h00ka'
+
+def send_webhook_message(new_amount):
+    message = f'BTC monke(s) found new home! {new_amount} left alone...'
+    req = requests.post(hook, json={'content': message})
+    if 200 <= req.status_code < 300:
+        print(f"Webhook sent {req.status_code}")
+    else:
+        print(f"Not sent with {req.status_code}, response:\n{req.json()}")
+
 
 # Path Chrome-Webdriver
 chrome_driver_path = 'chromedriver.exe'
 
 # URL where to scrape
-url = 'https://ordinalsbot.com/mint/btc-artsy-monke'
+scrape_url = 'https://ordinalsbot.com/mint/btc-artsy-monke'
 
 def ord_scraper():
     # Start Chrome
     driver = webdriver.Chrome(chrome_driver_path)
     driver.minimize_window()
-    driver.get(url)
+    driver.get(scrape_url)
 
     # wait for up to 10 seconds for the element to be visible
     wait = WebDriverWait(driver, 10)
@@ -46,18 +42,14 @@ def ord_scraper():
     print('Going into endless loop')
     try:
         while True:
-            sleep(10)
+            sleep(1.25)
             elements = driver.find_element(By.CSS_SELECTOR, "p.detail")
             new_available_count = int(elements.text.split(': ')[-1].split(' ')[0])
-            print('doing something here')
+            print(f'Available Monke: {available_count}')
             if new_available_count != available_count:
-                print(f'Available Monke: {new_available_count}')
                 available_count = new_available_count
-                result= requests.post(url, json=data, headers=headers)
-                if 200 <= result.status_code < 300:
-                    print(f"Webhook sent {result.status_code}")
-                else:
-                    print(f"Not sent with {result.status_code}, response:\n{result.json()}")
+                send_webhook_message(new_available_count)
+
     except:
         print("Something went wrong, send the highly trained apes!")
     sleep(10)
