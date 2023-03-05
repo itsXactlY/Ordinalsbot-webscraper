3d4c54b21f81400cebff1bc7f4249731ec404917
itsXactlY <6356217+itsXactlY@users.noreply.github.com>
2023-03-05 07:36:24 +0100

    Update ord_minter_go_brrrr.py

--- ord_minter_go_brrrr.py
+++ ord_minter_go_brrrr.py
@@ -6,29 +6,29 @@ from time import sleep
 import requests
 import schedule
 
-# preparing alerts (quick 'n dirty right now)
-hook = 'https://discord.com/api/webhooks/create/your-own-h00ka'
+# URL where to scrape
+mint_url = 'https://ordinalsbot.com/mint/btc-artsy-monke'
+
+# Webhook URL (Discod, Telegram, Weather Forecast...)
+webhook_url = 'https://discord.com/api/webhooks/create/your-own-h00k'
+
 
 def send_webhook_message(new_amount):
-    message = f'BTC monke(s) found new home! {new_amount} left alone...'
-    req = requests.post(hook, json={'content': message})
+    message = f'BTC monke(s) found new home! {new_amount} monke left alone...'
+    req = requests.post(webhook_url, json={'content': message})
     if 200 <= req.status_code < 300:
         print(f"Webhook sent {req.status_code}")
     else:
         print(f"Not sent with {req.status_code}, response:\n{req.json()}")
 
-
-# Path Chrome-Webdriver
-chrome_driver_path = 'chromedriver.exe'
-
-# URL where to scrape
-scrape_url = 'https://ordinalsbot.com/mint/btc-artsy-monke'
-
 def ord_scraper():
+    # Path Chrome-Webdriver
+    chrome_driver_path = 'chromedriver.exe'
+    
     # Start Chrome
     driver = webdriver.Chrome(chrome_driver_path)
     driver.minimize_window()
-    driver.get(scrape_url)
+    driver.get(mint_url)
 
     # wait for up to 10 seconds for the element to be visible
     wait = WebDriverWait(driver, 10)
@@ -38,21 +38,17 @@ def ord_scraper():
     available_count = int(element.text.split(': ')[-1].split(' ')[0])
     print(f'Available Monke: {available_count}')
 
-    sleep(3)
-    print('Going into endless loop')
     try:
         while True:
             sleep(1.25)
             elements = driver.find_element(By.CSS_SELECTOR, "p.detail")
             new_available_count = int(elements.text.split(': ')[-1].split(' ')[0])
-
             if new_available_count != available_count:
                 available_count = new_available_count
                 send_webhook_message(new_available_count)
 
     except:
-        print("Something went wrong, send the highly trained apes!")
-    sleep(10)
+        print("Something went wrong - send the highly trained apes to halp!")
 
 
 # schedule it
